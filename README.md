bef1e3deccc84b6b8ba35ea56ac6f1a75c29f4e8
Uday Kumar <udaykumaruking@gmail.com>
2020-09-21 20:38:31 +0530

    stateful widget done

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ This is for my personal reference
 - **Visible Widgets:** These widgets are drawn onto the screen in short these are the widgets which user sees. Such as _RaisedButton()_, _Text()_, _Card()_
 
 - **InVisible Widgets:** These widgets gives your app structure and control and it says how visible widgets are drawn onto the screen. Such as _Row()_, _Column()_, _ListView()_
+
+### Stateless Widget
+
+In this widget UI is rerendered when input data changes
+
+### Stateful Widget
+
+In this widget UI is rerendered when input data or local state changes, It has an internal state
--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 // We can use arrow functions like above if it has only one statement to be executed
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print("Answer Chosen!");
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TOD: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+
+    print(_questionIndex);
+    //print("Answer Chosen!");
   }
 
   // Each class shoud extend either stateless of stateful widget since each and everything in flutter is a widget.
@@ -17,8 +32,8 @@ class MyApp extends StatelessWidget {
   // App works without @override funtion too but its used for clearing doubts and styling purposes. It says that we are deliberately overriding stateless widget build function
   Widget build(BuildContext context) {
     var questions = [
-      'What\s your favorite color',
-      'What\s your favorite animal',
+      'What\'s your favorite color',
+      'What\'s your favorite animal',
     ];
     return MaterialApp(
       home: Scaffold(
@@ -29,10 +44,10 @@ class MyApp extends StatelessWidget {
           body: Column(
             // We can pass only one widget to body
             children: <Widget>[
-              Text("Question 1"),
-              RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion),
-              RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion),
-              RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion),
+              Text(questions[_questionIndex]),
+              RaisedButton(child: Text("Answer 1"), onPressed: _answerQuestion),
+              RaisedButton(child: Text("Answer 2"), onPressed: _answerQuestion),
+              RaisedButton(child: Text("Answer 3"), onPressed: _answerQuestion),
             ],
           )
           // Widget in angular brackets says the list - "[]" is of widget type
